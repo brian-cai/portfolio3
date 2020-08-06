af1296e33b72d5a56a4a209082b858a7e8211b9a
brian-cai <briancai@gatech.edu>
2020-08-06 16:18:04 -0700

    Updates
    Create CNAME
    
    Updates
    
    Updates
    
    Create CNAME
    
    Delete CNAME
    
    Deleting CNAME
    
    Create CNAME
    
    Update CNAME
    
    Updates
    
    Create CNAME
    
    Updates

---
+++ precache-manifest.9d3771c4e4e860f64ca763fccbfb9faa.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b760210260aff82e4382bd5cd4ee083",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cadbbd7bee446711762",
+    "url": "/static/css/2.151934bf.chunk.css"
+  },
+  {
+    "revision": "7151e1b75e8cb5365e26",
+    "url": "/static/css/main.0a8bc928.chunk.css"
+  },
+  {
+    "revision": "9cadbbd7bee446711762",
+    "url": "/static/js/2.2ff10a75.chunk.js"
+  },
+  {
+    "revision": "b3c7ab65ef83e1c487345dfc3362e427",
+    "url": "/static/js/2.2ff10a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7151e1b75e8cb5365e26",
+    "url": "/static/js/main.52667625.chunk.js"
+  },
+  {
+    "revision": "c85ac9ada01314dec032",
+    "url": "/static/js/runtime-main.4b148dc9.js"
+  },
+  {
+    "revision": "65b796335191db46f6ba7c0583cf36d2",
+    "url": "/static/media/01Arthur88Remise.65b79633.mp4"
+  },
+  {
+    "revision": "6e467fa8a320362fe0467cd48e2eba44",
+    "url": "/static/media/02RiposteBehind.6e467fa8.mp4"
+  },
+  {
+    "revision": "e98c04b2ec5a3430d744267243d77ec0",
+    "url": "/static/media/03Beat6.e98c04b2.mp4"
+  },
+  {
+    "revision": "92e6dbd09e3b2f83f86c35ea3dcefd2d",
+    "url": "/static/media/04AnthonyHoldLine.92e6dbd0.mp4"
+  },
+  {
+    "revision": "d0db410b4a5b937677dcee5bf15ff7f8",
+    "url": "/static/media/05KendrickDuckRemise.d0db410b.mp4"
+  },
+  {
+    "revision": "c8333c713a5de50e80c87d6016a08dd3",
+    "url": "/static/media/06Arthur8Bind.c8333c71.mp4"
+  },
+  {
+    "revision": "34df13f4f47c065c3550c3b960423d7a",
+    "url": "/static/media/07StefanoLate4.34df13f4.mp4"
+  },
+  {
+    "revision": "28bc6153fe7e17b1a83090b130f048c0",
+    "url": "/static/media/08Kendrick8Dodge.28bc6153.mp4"
+  },
+  {
+    "revision": "805ba8cb6bc19f54431c95bcdcefa4d7",
+    "url": "/static/media/Stepmania.805ba8cb.png"
+  },
+  {
+    "revision": "720de4a8dad67cdaa6ab757f1bfca31f",
+    "url": "/static/media/Youtube.720de4a8.svg"
+  },
+  {
+    "revision": "2248542e1bbbd548a157e3e6ced054fc",
+    "url": "/static/media/fa-brands-400.2248542e.woff"
+  },
+  {
+    "revision": "3654744dc6d6c37c9b3582b57622df5e",
+    "url": "/static/media/fa-brands-400.3654744d.woff2"
+  },
+  {
+    "revision": "748ab466bee11e0b2132916def799916",
+    "url": "/static/media/fa-brands-400.748ab466.eot"
+  },
+  {
+    "revision": "7febe26eeb4dd8e3a3c614a144d399fb",
+    "url": "/static/media/fa-brands-400.7febe26e.ttf"
+  },
+  {
+    "revision": "b032e14eac87e3001396ff597e4ec15f",
+    "url": "/static/media/fa-brands-400.b032e14e.svg"
+  },
+  {
+    "revision": "33f727ccde4b05c0ed143c5cd78cda0c",
+    "url": "/static/media/fa-regular-400.33f727cc.woff2"
+  },
+  {
+    "revision": "3929b3ef871fa90bbb4e77e005851e74",
+    "url": "/static/media/fa-regular-400.3929b3ef.svg"
+  },
+  {
+    "revision": "54f142e03adc6da499c2af4f54ab76fd",
+    "url": "/static/media/fa-regular-400.54f142e0.ttf"
+  },
+  {
+    "revision": "b58f468f84168d61e0ebc1e1f423587c",
+    "url": "/static/media/fa-regular-400.b58f468f.eot"
+  },
+  {
+    "revision": "f3dd4f397fbc5aaf831b6b0ba112d75c",
+    "url": "/static/media/fa-regular-400.f3dd4f39.woff"
+  },
+  {
+    "revision": "035a137af03db6f1af76a589da5bb865",
+    "url": "/static/media/fa-solid-900.035a137a.eot"
+  },
+  {
+    "revision": "6661d6b3521b4c480ba759e4b9e480c1",
+    "url": "/static/media/fa-solid-900.6661d6b3.woff"
+  },
+  {
+    "revision": "8a8c0474283e0d9ef41743e5e486bf05",
+    "url": "/static/media/fa-solid-900.8a8c0474.woff2"
+  },
+  {
+    "revision": "9bbbee00f65769a64927764ef51af6d0",
+    "url": "/static/media/fa-solid-900.9bbbee00.svg"
+  },
+  {
+    "revision": "b6a14bb88dbc580e45034af297c8f605",
+    "url": "/static/media/fa-solid-900.b6a14bb8.ttf"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  }
+]);
