800cf96ba8f2d5de2786fbe7605bdfd9b31bee4a
brian-cai <thebriancai@gmail.com>
2021-08-18 02:53:38 -0700

    Source Code URL

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,7 +10,22 @@ class Footer extends Component {
                          marginTop: "20vh",
                          marginBottom: "4vh"
                      }}>
-
+                <div class="h2">
+                    <a target="_blank" 
+                    rel="noopener noreferrer" 
+                    style={{
+                        zIndex: 5,
+                        width: "100%",
+                        margin: "0 auto",
+                        textAlign: "50%",
+                        backgroundColor:"transparent",
+                        color: "white",
+                        opacity: 0.5
+                    }}
+                    href="https://github.com/brian-cai/portfolio3/tree/sourcecode"> 
+                        Source Code on Github 
+                    </a>
+                </div>
                 <div class="h1" style={{
                     zIndex: 5,
                     width: "100%",
